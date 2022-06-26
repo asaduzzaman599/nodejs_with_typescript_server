274e1bedd1f1b5a49809a8245d7f3f03587281ce
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-06-26 16:20:52 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
+const mongodb_1 = require("mongodb");
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
+dotenv_1.default.config();
 //middleware
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+//mongoDB
+const option = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: mongodb_1.ServerApiVersion.v1 };
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuwrq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new mongodb_1.MongoClient(uri, option);
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.connect();
+        console.log("db connected");
+        
+    }
+    finally {
+    }
+});
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send(`server running port: ${PORT}`);
 });
--- index.ts
+++ index.ts
@@ -2,12 +2,33 @@ import express,{ Express, Request, Response } from  'express';
 import dotenv from 'dotenv'
 import cors from 'cors';
 
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { runMain } from 'module';
+
 const app:Express = express();
 const PORT:string|number =   process.env.PORT||5000;
+dotenv.config()
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//mongoDB
+const option =  { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+const uri:string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yuwrq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client:MongoClient = new MongoClient(uri,option);
+
+
+const run = async ()=>{
+    try{
+       await client.connect()
+        console.log("db connected")
+    }finally{
+        
+    }
+}
+run().catch(console.dir)
 
 app.get('/',(req:Request,res:Response):void=>{
     res.send(`server running port: ${PORT}`)
