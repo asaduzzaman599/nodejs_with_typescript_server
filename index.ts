4f87792a20cd51ea4255e139d3ce0840435a5ab1
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-06-26 15:40:15 +0600

    first server running with type script successfully

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
 import express,{ Express, Request, Response } from  'express';
+import dotenv from 'dotenv'
+import cors from 'cors';
+
+const app:Express = express();
+const PORT:string|number =   process.env.PORT||5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req:Request,res:Response):void=>{
+    res.send(`server running port: ${PORT}`)
+})
+
+app.listen(PORT,():void=>{
+    console.log(`server running port: ${PORT}`)
+})
+
